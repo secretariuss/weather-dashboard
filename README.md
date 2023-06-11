305799a5a717d4b8e9f1c5314d8a91f3b6be99ce
secretariuss <secretarius@ukr.net>
2023-06-11 11:05:33 -0300

    added some function and structure div

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Work Day Scheduler Starter Code
 
 ## User Story
-AS AN employee with a busy schedule
-I WANT to add important events to a daily planner
-SO THAT I can manage my time effectively
+AS A traveler
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
 
 ## Description
 
@@ -19,19 +19,14 @@ https://
 The following scrshots shows the web application's:
 
 ![scrshot1](./assets/images/pg1.PNG)
-![scrshot2](./assets/images/pg2.PNG)
 
 ## Acceptance Criteria
-GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
-WHEN I scroll down
-THEN I am presented with time blocks for standard business hours of 9am to 5pm
-WHEN I view the time blocks for that day
-THEN each time block is color-coded to indicate whether it is in the past, present, or future
-WHEN I click into a time block
-THEN I can enter an event
-WHEN I click the save button for that time block
-THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
--- script.js
+++ script.js
@@ -1,240 +1,197 @@
-var container = $('#container')
 var date = dayjs();
-var currentHourId = "hour-" + dayjs().get('hour');
-var cutterntDay = $('#currentDay');
-var saveBtn = document.querySelector('.saveBtn');
-var currentHours = dayjs().get('hour');
+console.log(date);
+var actualWeather = $('#actual-weather');
+var cardContainer = $('#card-container');
+var APIKey = "810f728d770cc413c5291598e605e72a";
+var card = $('.card')
 
-var showCurrentDay = function () {
-  var date = dayjs();
-  $('#currentDay').text(date.format('dddd, MMMM D') + 'th');
+// var cutterntDay = $('#currentDay');
+var searchBtn = document.querySelector('.search-button');
+
+
+var inputEl = document.querySelector('.search-city');
+
+var city = localStorage.getItem('cityNameStore');
+
+var actualWeatherDiv = [
+  '<h3 id="city" class="city-name mb-1 mt-2 me-2 bolder"></h3><p id="currentDay" class="lead"></p>',
+  '<p id="weather-icon">',
+  '<p>Temp: <span class="actual temp" id="temp"></span></p>',
+  '<p>Wind: <span class="actual" id="wind"></span></p>',
+  '<p>Humidity: <span class="actual" id="humidity"></span></p>',
+]
+
+// var forecastDivs = [
+//   '<div id="card0" class="col-sm-2 bg-primary forecast text-white ml-2 mb-3 p-2 mt-2 rounded"></div>',
+//   '<p id="date0"></p>',
+//   '<p id="img0"></p>',
+//   '<p>Temp:<span id="temp0"></span></p>',
+//   '<p>Wind:<span id="wind0"></span></p>',
+//   '<p>Humidity:<span id="humidity0"></span></p>',
+// ]
+
+var cardDivs = [
+  '<p id="date0"></p>',
+  '<p id="img0"></p>',
+  '<p>Temp:<span id="temp0"></span></p>',
+  '<p>Wind:<span id="wind0"></span></p>',
+  '<p>Humidity:<span id="humidity0"></span></p>',
+]
+
+  var showCurrentDay = function () {
+    var date = dayjs();
+    $('.lead').text('(' + date.format('DD/MM/YYYY') + ')');
+  }
+
+function setCityToLocalStorage() {
+  localStorage.setItem('cityNameStore', inputEl.value);
 }
 
-function createDiv1() {
-
-  $("<div>", {
-    'id': "hour-9",
-    'class': "row time-block",
-  }).appendTo(this.container)
-    .append($("<div>", {
-      'text': "9AM",
-      'class': "col-2 col-md-1 hour text-center py-3",
-    }))
-    .append($("<textarea>", {
-      'rows': "3",
-      'class': "col-8 col-md-10 description",
-    }))
-    .append($("<button>", {
-      'aria-label': "save",
-      'class': "btn saveBtn col-2 col-md-1",
-    }).append($("<i>", {
-      'class': "fas fa-save",
-      'aria-hidden': "true"
-    })))
-
-  saveInput();
+function actualWeatherContent() {
+  for (div of actualWeatherDiv) {
+    console.log(div);
+    $(div).appendTo(this.actualWeather)    
+  }
 }
 
-function createDiv2() {
+// function createCardDivs() {
+//   for(x=0; x<5; x++) {
+//     $("<div>", {
+//       'id': "card",
+//       'class': "col-sm-2 bg-primary forecast text-white me-2 mb-3 p-2 mt-2 rounded"}).appendTo(this.cardContainer)    
+//     }
+// }
 
-  $("<div>", {
-    'id': "hour-10",
-    'class': "row time-block",
-  }).appendTo(this.container)
-    .append($("<div>", {
-      'text': "10AM",
-      'class': "col-2 col-md-1 hour text-center py-3",
-    }))
-    .append($("<textarea>", {
-      'rows': "3",
-      'class': "col-8 col-md-10 description",
-    }))
-    .append($("<button>", {
-      'aria-label': "save",
-      'class': "btn saveBtn col-2 col-md-1",
-    }).append($("<i>", {
-      'class': "fas fa-save",
-      'aria-hidden': "true"
-    })))
-
-  saveInput();
+function createCard0() {
+  for (div of cardDivs) {
+    $(div).appendTo(this.card)    
+  }
+  // $("<div>", {
+  //   'id': "card0",
+  //   'class': "col-sm-2 bg-primary forecast text-white me-2 mb-3 p-2 mt-2 rounded",
+  // }).appendTo(this.cardContainer)
+  //   .append($("<p>"), {
+  //     'id': "date0"
+  //   })
+  //   .append($("<p>"), {
+  //     'id': "img0"
+  //   })
+  //   .append($("<p>")).append($("<span>", {
+  //     'id': "temp0",
+  //     'text': "Temp:"
+  //   }))
+  //   .append($("<p>")).append($("<span>", {
+  //     'id': "wind0",
+  //     'text': "Wind:"
+  //   }))
+  //   .append($("<p>")).append($("<span>", {
+  //     'id': "humidity0",
+  //     'text': "Humidity:"
+  //   }))
 }
+// function createCard1() {
 
-function createDiv3() {
+//   $("<div>", {
+//     'id': "card1",
+//     'class': "col-sm-2 bg-primary forecast text-white me-2 mb-3 p-2 mt-2 rounded",
+//   }).appendTo(this.cardContainer)
+//     .append($("<p>"), {
+//       'id': "date1"
+//     })
+//     .append($("<p>"), {
+//       'id': "img1"
+//     })
+//     .append($("<p>")).append($("<span>", {
+//       'id': "temp1",
+//       'text': "Temp:"
+//     }))
+//     .append($("<p>")).append($("<span>", {
+//       'id': "wind1",
+//       'text': "Wind:"
+//     }))
+//     .append($("<p>")).append($("<span>", {
+//       'id': "humidity1",
+//       'text': "Humidity:"
+//     }))
+// }
 
-  $("<div>", {
-    'id': "hour-11",
-    'class': "row time-block",
-  }).appendTo(this.container)
-    .append($("<div>", {
-      'text': "11AM",
-      'class': "col-2 col-md-1 hour text-center py-3",
-    }))
-    .append($("<textarea>", {
-      'rows': "3",
-      'class': "col-8 col-md-10 description",
-    }))
-    .append($("<button>", {
-      'aria-label': "save",
-      'class': "btn saveBtn col-2 col-md-1",
-    }).append($("<i>", {
-      'class': "fas fa-save",
-      'aria-hidden': "true"
-    })))
+// function createCard2() {
 
-  saveInput();
-}
+//   $("<div>", {
+//     'id': "card2",
+//     'class': "col-sm-2 bg-primary forecast text-white me-2 mb-3 p-2 mt-2 rounded",
+//   }).appendTo(this.cardContainer)
+//     .append($("<p>"), {
+//       'id': "date2"
+//     })
+//     .append($("<p>"), {
+//       'id': "img2"
+//     })
+//     .append($("<p>")).append($("<span>", {
+//       'id': "temp2",
+//       'text': "Temp:"
+//     }))
+//     .append($("<p>")).append($("<span>", {
+//       'id': "wind2",
+//       'text': "Wind:"
+//     }))
+//     .append($("<p>")).append($("<span>", {
+//       'id': "humidity2",
+//       'text': "Humidity:"
+//     }))
+// }
 
-function createDiv4() {
+// function createCard3() {
 
-  $("<div>", {
-    'id': "hour-12",
-    'class': "row time-block",
-  }).appendTo(this.container)
-    .append($("<div>", {
-      'text': "12PM",
-      'class': "col-2 col-md-1 hour text-center py-3",
-    }))
-    .append($("<textarea>", {
-      'rows': "3",
-      'class': "col-8 col-md-10 description",
-    }))
-    .append($("<button>", {
-      'aria-label': "save",
-      'class': "btn saveBtn col-2 col-md-1",
-    }).append($("<i>", {
-      'class': "fas fa-save",
-      'aria-hidden': "true"
-    })))
+//   $("<div>", {
+//     'id': "card3",
+//     'class': "col-sm-2 bg-primary forecast text-white me-2 mb-3 p-2 mt-2 rounded",
+//   }).appendTo(this.cardContainer)
+//     .append($("<p>"), {
+//       'id': "date3"
+//     })
+//     .append($("<p>"), {
+//       'id': "img3"
+//     })
+//     .append($("<p>")).append($("<span>", {
+//       'id': "temp3",
+//       'text': "Temp:"
+//     }))
+//     .append($("<p>")).append($("<span>", {
+//       'id': "wind3",
+//       'text': "Wind:"
+//     }))
+//     .append($("<p>")).append($("<span>", {
+//       'id': "humidity3",
+//       'text': "Humidity:"
+//     }))
+// }
 
-  saveInput();
-}
+// function createCard4() {
 
-function createDiv5() {
-
-  $("<div>", {
-    'id': "hour-13",
-    'class': "row time-block",
-  }).appendTo(this.container)
-    .append($("<div>", {
-      'text': "1PM",
-      'class': "col-2 col-md-1 hour text-center py-3",
-    }))
-    .append($("<textarea>", {
-      'rows': "3",
-      'class': "col-8 col-md-10 description",
-    }))
-    .append($("<button>", {
-      'aria-label': "save",
-      'class': "btn saveBtn col-2 col-md-1",
-    }).append($("<i>", {
-      'class': "fas fa-save",
-      'aria-hidden': "true"
-    })))
-
-  saveInput();
-}
-
-
-function createDiv6() {
-
-  $("<div>", {
-    'id': "hour-14",
-    'class': "row time-block",
-  }).appendTo(this.container)
-    .append($("<div>", {
-      'text': "2PM",
-      'class': "col-2 col-md-1 hour text-center py-3",
-    }))
-    .append($("<textarea>", {
-      'rows': "3",
-      'class': "col-8 col-md-10 description",
-    }))
-    .append($("<button>", {
-      'aria-label': "save",
-      'class': "btn saveBtn col-2 col-md-1",
-    }).append($("<i>", {
-      'class': "fas fa-save",
-      'aria-hidden': "true"
-    })))
-
-  saveInput();
-}
-
-function createDiv7() {
-
-  $("<div>", {
-    'id': "hour-15",
-    'class': "row time-block",
-  }).appendTo(this.container)
-    .append($("<div>", {
-      'text': "3PM",
-      'class': "col-2 col-md-1 hour text-center py-3",
-    }))
-    .append($("<textarea>", {
-      'rows': "3",
-      'class': "col-8 col-md-10 description",
-    }))
-    .append($("<button>", {
-      'aria-label': "save",
-      'class': "btn saveBtn col-2 col-md-1",
-    }).append($("<i>", {
-      'class': "fas fa-save",
-      'aria-hidden': "true"
-    })))
-
-  saveInput();
-}
-
-function createDiv8() {
-
-  $("<div>", {
-    'id': "hour-16",
-    'class': "row time-block",
-  }).appendTo(this.container)
-    .append($("<div>", {
-      'text': "4PM",
-      'class': "col-2 col-md-1 hour text-center py-3",
-    }))
-    .append($("<textarea>", {
-      'rows': "3",
-      'class': "col-8 col-md-10 description",
-    }))
-    .append($("<button>", {
-      'aria-label': "save",
-      'class': "btn saveBtn col-2 col-md-1",
-    }).append($("<i>", {
-      'class': "fas fa-save",
-      'aria-hidden': "true"
-    })))
-
-  saveInput();
-}
-
-function createDiv9() {
-
-  $("<div>", {
-    'id': "hour-17",
-    'class': "row time-block",
-  }).appendTo(this.container)
-    .append($("<div>", {
-      'text': "5PM",
-      'class': "col-2 col-md-1 hour text-center py-3",
-    }))
-    .append($("<textarea>", {
-      'rows': "3",
-      'class': "col-8 col-md-10 description",
-    }))
-    .append($("<button>", {
-      'aria-label': "save",
-      'class': "btn saveBtn col-2 col-md-1",
-    }).append($("<i>", {
-      'class': "fas fa-save",
-      'aria-hidden': "true"
-    })))
-
-  saveInput();
-}
+//   $("<div>", {
+//     'id': "card4",
+//     'class': "col-sm-2 bg-primary forecast text-white me-2 mb-3 p-2 mt-2 rounded",
+//   }).appendTo(this.cardContainer)
+//     .append($("<p>"), {
+//       'id': "date4"
+//     })
+//     .append($("<p>"), {
+//       'id': "img4"
+//     })
+//     .append($("<p>")).append($("<span>", {
+//       'id': "temp4",
+//       'text': "Temp:"
+//     }))
+//     .append($("<p>")).append($("<span>", {
+//       'id': "wind4",
+//       'text': "Wind:"
+//     }))
+//     .append($("<p>")).append($("<span>", {
+//       'id': "humidity4",
+//       'text': "Humidity:"
+//     }))
+// }
 
 //only for example always current hour
 // function alwaysCurrentHour() {
@@ -281,35 +238,82 @@ function getHours() {
 
 }
 
-function saveInput() {
+// function saveInput() {
 
-  $('.saveBtn').on('click', function () {
-    var hours = $(this).parent().attr("id");
-    console.log(hours);
-    var textAreaValue = $(this).siblings('.description').val();
-    localStorage.setItem(hours, textAreaValue);
-    console.log(textAreaValue);
+//   $('.saveBtn').on('click', function () {
+//     var hours = $(this).parent().attr("id");
+//     console.log(hours);
+//     var textAreaValue = $(this).siblings('.description').val();
+//     localStorage.setItem(hours, textAreaValue);
+//     console.log(textAreaValue);
+//   })
+
+//   $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+//   $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+//   $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+//   $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+//   $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+//   $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+//   $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+//   $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+// }
+
+function getApi() {
+  // fetch request gets a list of all the repos for the node.js organization
+  //var requestUrl = `http://api.openweathermap.org/geo/1.0/reverse?lat=${51.5073219}&lon=${-0.1276474}&appid=${APIKey}`;
+  var queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+
+   fetch(queryURL)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    $('.city-name').html("<h3>" + data.name + "</h3>");
+    $('#weather-icon').html("<img src='https://openweathermap.org/img/w/" + data.weather[0].icon + ".png' >");
+    $('#wind').text(data.wind.speed + " MPH");
+    $('#humidity').text(data.main.humidity + "%");
+    $("#temp").text(data.main.temp + " F");
+    var lat = data.coord.lat;
+    var lon = data.coord.lon;
+    var queryURLUv = `http://api.openweathermap.org/data/2.5/uvi?&appid=${APIKey}&lat=${lat}&lon=${lon}`;
+    fetch(queryURLUv)
+    .then((g)=> g.json())
+    .then(c => console.log(c.value))
   })
 
-  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
-  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
-  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
-  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
-  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
-  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
-}
+  }
 
-createDiv1();
-createDiv2();
-createDiv3();
-createDiv4();
-createDiv5();
-createDiv6();
-createDiv7();
-createDiv8();
-createDiv9();
-//alwaysCurrentHour();
+  // function getURL() {
+    
+  //   var queryURLUv = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + apiKey;
+  //   fetch(queryURLUv)
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     var lat = data.coord.lat;
+  //     var lon = data.coord.lon;
+      
+  //    console.log(lat, lon);
+  //     })
+  // }
+
+  
+// function getApi() {
+//   var requestUrl = "https://api.openweathermap.org/data/3.0/onecall?lat=${51.5073219}lon=4{-0.1276474}appid=810f728d770cc413c5291598e605e72a";
+  
+//   fetch(requestUrl)
+//   .then((response) => response.json())
+//   .then((data) => console.log(data))
+// }
+
+createCard0()
+// createCard1()
+// createCard2()
+// createCard3()
+// createCard4()
+actualWeatherContent();
+
 getHours();
+getApi();
+//createCardDivs();
 showCurrentDay();
+
+searchBtn.addEventListener('click', setCityToLocalStorage);
